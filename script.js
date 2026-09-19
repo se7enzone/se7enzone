cb1fa35dc1f9d56f6ffcb226f2297dafc13dfc27
se7enzone <muhammadainuroiq@gmail.com>
2026-09-20 00:49:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// Fungsi untuk mengganti status aktif pada tombol kapsul
+function switchTab(clickedButton) {
+    // Ambil semua tombol kapsul
+    const tabs = document.querySelectorAll('.tab-pill');
+    
+    // Hapus kelas 'active' dari semua tombol
+    tabs.forEach(tab => tab.classList.remove('active'));
+    
+    // Tambahkan kelas 'active' hanya ke tombol yang sedang diklik
+    clickedButton.classList.add('active');
+}
